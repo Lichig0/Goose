813d03557373cebb1c9fe2c147d191b6db7bb999
Lichig0 <sorda4@gmail.com>
2020-07-23 14:48:22 -0400

    Add guildMemberAdd and messageDelete events

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,7 @@
+module.exports = (client, member) => {
+  console.log(`${member} joined.`);
+  const role = member.guild.roles.find('name', 'Member');
+  member.addRole(role);
+  // Send the message, mentioning the member
+  // channel.send(`${member}[${member.user.username}] Joined.`);
+}
---
+++ events/messageDelete.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+
+module.exports = (client, messageDelete) => {
+  if (messageDelete.attachments && messageDelete.attachments.size > 0 && messageDelete) { // If I change this to: message.attachments.size>0 && message it works with deleted image & text but as it is without this said line it doesn't function
+
+    var Attachment = (messageDelete.attachments).array();
+
+    Attachment.forEach(function (attachment) {
+      const logembed = new Discord.MessageEmbed()
+
+        .setAuthor(messageDelete.author.tag, messageDelete.author.displayAvatarURL)
+        .setDescription(`**Image sent by ${messageDelete.author.tag} deleted in <#${messageDelete.channel.id}>**`)
+        .setImage(attachment.proxyURL)
+
+        .setColor(messageDelete.guild.member(client.user).displayHexColor)
+
+        .setFooter(`Deleted Image`)
+        .setTimestamp()
+
+      messageDelete.channel.send(logembed);
+      console.log(attachment.proxyURL);
+    })
+  } else {
+    console.log(messageDelete)
+    const logembed = new Discord.MessageEmbed()
+      //.setTitle('Message Deleted')
+      .setAuthor(messageDelete.author.tag, messageDelete.author.displayAvatarURL)
+      .setDescription(`**Message sent by ${messageDelete.author.tag} deleted in <#${messageDelete.channel.id}>**`)
+      .addField("Message Content", `${messageDelete.content}`)
+
+      .setColor(messageDelete.guild.member(client.user).displayHexColor)
+
+      .setFooter(`Deleted Message`)
+      .setTimestamp()
+
+    messageDelete.channel.send(logembed);
+
+  }
+}
