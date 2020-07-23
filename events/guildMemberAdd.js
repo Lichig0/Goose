0bf524c57435427b621d67280c3f03cc8bc1fce6
Lichig0 <sorda4@gmail.com>
2020-07-23 19:45:48 -0400

    Look in the cache for guild roles.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 module.exports = (client, member) => {
   console.log(`${member} joined.`);
-  const role = member.guild.roles.find('name', 'Member');
+  const role = member.guild.roles.cache.find('name', 'Member');
   member.addRole(role);
   // Send the message, mentioning the member
   // channel.send(`${member}[${member.user.username}] Joined.`);
