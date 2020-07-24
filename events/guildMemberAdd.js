48b7676215a493da38d2c08c42e7854ad43b189b
Lichig0 <sorda4@gmail.com>
2020-07-24 09:25:46 -0400

    add role if exists

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,8 @@
 module.exports = (client, member) => {
   console.log(`${member} joined.`);
   const role = member.guild.roles.cache.find('name', 'Member');
-  member.addRole(role);
+  console.log(`found ${role}`)
+  if (role !== undefined) member.addRole(role);
   // Send the message, mentioning the member
   // channel.send(`${member}[${member.user.username}] Joined.`);
 }
