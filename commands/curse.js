d57ec6041f3a60fe0b1b4344e10bebc20e489e9b
Lichig0 <sorda4@gmail.com>
2020-07-24 13:05:35 -0400

    curse everyone

--- commands/curse.js
+++ commands/curse.js
@@ -2,18 +2,24 @@ const { Permissions } = require("discord.js")
 
 module.exports = (message, epeen, who = undefined) => {
     const role_perm = epeen.has(Permissions.FLAGS.MANAGE_ROLES);
-    const member = who || message.mentions.members.first();
-    if (!member) {
+    let members = who || message.mentions.members;
+    if(message.mentions.everyone) {
+        members = message.channel.members;
+    }
+    if (!members || !message.guild) {
         return;
     }
-    if (!member.manageable || !role_perm) {
-        return message.reply(`Honk.`)
-    }
+
     let role = message.guild.roles.cache.find(r => r.name === "cursed")
-    if (!message.content.includes('lift')) {
-        return member.roles.add(role);
-    } else {
-        return member.roles.remove(role);
-    }
+    members.array().forEach(member => {
+        if (!member.manageable || !role_perm) {
+            return message.channel.send(`***Honk.*** (${member.user.username})`)
+        }
+        if (!message.content.includes('lift')) {
+            return member.roles.add(role);
+        } else {
+            return member.roles.remove(role);
+        }
+    });
 
 }
