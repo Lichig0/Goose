bcfd3f632681dca93e1a9e9db407a28bca2b7761
Lichig0 <sorda4@gmail.com>
2020-08-04 17:10:19 -0400

    Fix adat

--- commands/adat.js
+++ commands/adat.js
@@ -1,18 +1,17 @@
 module.exports = (message) => {
-  const {author, channel} = message;
-  if (author.kickable === false) {
-    return message.reply(`No.`)
-  }
-  return author
-    .kick()
-    .then(() => {
+  const {author, channel, guild} = message;
+  guild.members.fetch(author).then(m => {
+    if (m.kickable === false) {
+      return message.reply('ABAT!');
+    }
+    m.kick().then(() => {
       console.log(message, author);
       channel.createInvite({ maxUses: 1 }).then(invite => {
         author.createDM().then(dm => {
           dm.send(`When you're not ADAT: ${invite.url}`)
         }).catch(error => console.error(error));
       }).catch(error => console.error(error));
-      channel.send(`ADAT! -${author.tag.tag}`)
-    })
-    .catch(error => message.reply(`Error.`))
+      channel.send(`ADAT! -${m.tag.tag}`)
+    }).catch(error => message.reply(`Error.`))
+  })
 }
