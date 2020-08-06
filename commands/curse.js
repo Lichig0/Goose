dcaf4c7af2840cc1a13574bc25c1faeb6579d6e6
Lichig0 <sorda4@gmail.com>
2020-08-05 21:33:45 -0400

    create curse role if it doesn't exist

--- commands/curse.js
+++ commands/curse.js
@@ -3,6 +3,7 @@ const { Permissions } = require("discord.js")
 module.exports = (message, epeen, who = undefined) => {
     const role_perm = epeen.has(Permissions.FLAGS.MANAGE_ROLES);
     let members = who || message.mentions.members;
+    const guild = message.guild;
     if(message.mentions.everyone) {
         members = message.channel.members;
     }
@@ -11,6 +12,24 @@ module.exports = (message, epeen, who = undefined) => {
     }
 
     let role = message.guild.roles.cache.find(r => r.name === "cursed")
+    if (!role) {
+        // Create a new role with data and a reason
+        guild.roles.create({
+            data: {
+                name: 'cursed',
+                color: '#2B2B2B',
+                position: 10,
+                permissions: new Permissions(327744),
+            },
+            reason: 'Sometimes you need to silence the people.',
+        }).then((r) => {
+            role = r;
+        }).catch((err) => {
+            console.error(err)
+            return;
+        });
+
+    }
     members.array().forEach(member => {
         if (!member.manageable || !role_perm) {
             return message.channel.send(`***Honk.*** (${member.user.username})`)
