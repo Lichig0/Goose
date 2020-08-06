fcae0fc2b4c640f72e1f1d0784b922e324661d41
Lichig0 <sorda4@gmail.com>
2020-08-05 21:34:11 -0400

    minor changes to member leave

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,11 +3,11 @@ module.exports = (client, member) => {
   const channel = member.guild.channels.cache.find(ch => ch.name === 'shitposting');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-  channel.createInvite({ maxUses: 1 }).then(invite => {
-    member.createDM().then(dm => {
-      dm.send(`*Continue?* ${invite.url}`);
-    }).catch(error => console.error(error));
-  }).catch(error => console.error(error));
   // Send the message, mentioning the member
   channel.send(`${member}[${member.user.username}] left.`);
+  member.createDM().then(dm => {
+    channel.createInvite({maxUses: 1}).then(invite => {
+      dm.send(`*Continue?* ${invite.url}`);
+    }).catch(error => {console.error(error)});
+  }).catch(error => { console.error(error) });
 }
