8d0d407b49f52768d326950978ba634d6c08f4ee
Lichig0 <sorda4@gmail.com>
2020-08-11 12:17:02 -0400

    changes to the honker donker

--- chatter/chatter.js
+++ chatter/chatter.js
@@ -1,8 +1,8 @@
 const Markov = require('markov-strings').default
 const fs = require('fs');
-const data = [{ string: 'honk' }];
+const data = {0:{ string: 'honk' }};
 let freq = process.env.FREQUENCY || 0.995;
-let markov = new Markov(data.flat(2), { stateSize: 2 });
+let markov = new Markov(Object.values(data).flat(2), { stateSize: 2 });
 let reload = 0;
 let config = {};
 
@@ -19,7 +19,7 @@ module.exports.run = (message, client) => {
   const honkChannel = (isMentioned && config.useHonk) ? theHonk : channel;
   //guild.channels.create('honk',{type: 'text', topic: 'honk', rateLimitPerUser: 1, reason: 'Channel for bot use without spaming other channels'});
   if (rand > 0.98 || reload <= 0) {
-    if (data.length == 1) data.pop();
+    if (data.length == 1) delete data['0'];
     const textChannels = guild.channels.cache.filter(ch => ch.type == 'text' && ch.viewable && !ch.nsfw);
     readMessages(message, textChannels);
     reload = 500;
@@ -47,7 +47,7 @@ const loadData =(client) => {
       return console.log(err);
     }
     data = JSON.parse(data);
-    markov = new Markov(data.flat(2), { stateSize: 2 })
+    markov = new Markov(Object.values(data).flat(2), { stateSize: 2 })
     markov.buildCorpusAsync().then(() => {
       // message.react('💡');
       client.user.setStatus('online');
@@ -63,7 +63,7 @@ const loadData =(client) => {
 
 const sendMarkovString = async (channel, data, content) => {
   channel.startTyping();
-  console.log('okay', data.length);
+  console.log('okay', Object.values(data).length);
   const includesWord = (word) => {
     console.log(content, word);
     return content.includes(word);
@@ -91,30 +91,42 @@ const sendMarkovString = async (channel, data, content) => {
 }
 
 
-const buildData = async (o = false, channels, data, times) => {
+const buildData = async (last = {}, channels, data, times) => {
   times++;
 
   // const msgs = await channel.messages.fetch({ limit: 100, before: o });
-  const tasks = channels.array().flatMap((ch) => fetchMessages(ch, o));
+  const tasks = channels.array().flatMap((ch) => fetchMessages(ch, last[ch.id]));
   const msgs = await Promise.all(tasks);
-  msgs.forEach(mm => {
+  const toCache = msgs.filter(m => m.size > 0)
+  toCache.filter(m => m.size > 0).forEach(mm => {
     mm.forEach((m, i, s) => {
       const multi = m.cleanContent.split(/[\n.;()]/);
-      const cache = { string: m.cleanContent, mid: m.id, guide: m.guild, channel: m.channel }
-      multi.forEach(str => {
-        cache.string = str;
-        data.push(cache);
+      const cache = { string: m.cleanContent, id: m.id, guild: m.guild.id, channel: m.channel.id }
+      multi.forEach((str, i) => {
+        if (str !== "") {
+          cache.string = str;
+          if (data[`${m.id}.${i}`] !== undefined) {
+            console.warn("Duplicate!", str, mm.size);
+            return;
+          } else {
+            data[`${m.id}.${i}`] = cache;
+          }
+        }
       })
-      last = cache
+      last[m.channel.id] = cache
     });
+    if (mm.size < 100) {
+      const i = msgs.indexOf(mm);
+      if ( i > -1 && msgs[i].array().size > 0) channels.delete(msgs[i].array()[0].channel.id);
+    }
   });
-  if (data.length < config.arrayLimiter && times < 500) {
+  if (Object.values(data).length < config.arrayLimiter && times < 500 && toCache.length > 0) {
     await buildData(last, channels, data, times);
   }
 }
 
 const fetchMessages = async (channel, o) => {
-  return o.channel === channel ? channel.messages.fetch({ limit: 100, before: o.id }) : channel.messages.fetch({ limit: 100 })
+  return o.channel === channel.id ? channel.messages.fetch({ limit: 100, before: o.id }) : channel.messages.fetch({ limit: 100 })
 }
 
 const readMessages = (message, textChannels) => {
@@ -122,11 +134,14 @@ const readMessages = (message, textChannels) => {
   let r = 0
   client.user.setStatus('dnd');
   console.log(`Reading messages`);
-  buildData(message.id, textChannels, data, r).then(() => {
+  const last = {};
+  textChannels.forEach(tc=> last[tc.id] = {});
+  last[message.channel.id] = message;
+  buildData(last, textChannels, data, r).then(() => {
   }).catch((err) => {
     console.error(err);
   }).finally(() => {
-    markov = new Markov(data.flat(2), config.chatter.corpus)
+    markov = new Markov(Object.values(data).flat(2), config.chatter.corpus)
     markov.buildCorpusAsync().then(() => {
       // message.react('💡');
       client.user.setStatus('online');
@@ -136,7 +151,7 @@ const readMessages = (message, textChannels) => {
       client.user.setStatus('idle');
       // message.react('😵');
     });
-    console.log('Done.', data.length);
+    console.log('Done.', Object.values(data).length);
   });
 }
 
