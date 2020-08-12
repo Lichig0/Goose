d1816fcf06fd1bb063ee104efd220dbe5aab68f2
Lichig0 <sorda4@gmail.com>
2020-08-12 12:06:56 -0400

    add whoami

---
+++ commands/whoami.js
@@ -0,0 +1,21 @@
+const {MessageEmbed} = require("discord.js");
+
+module.exports.run = (message) => {
+  const { author } = message;
+
+  const logembed = new MessageEmbed()
+    .setAuthor(author.tag)
+    .setDescription(`**Details of ${author.tag} **`)
+    .setTimestamp();
+    for(prop in author) {
+      if(prop === 'flags') continue;
+      logembed.addField(prop,author[prop]);
+    }
+    author.createDM().then(dm => {
+      dm.send(logembed)
+    }).catch(error => console.error(error));
+}
+
+const help = () => `PM you some details about you.\n`
+
+exports.help = help;
