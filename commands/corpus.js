943575d83f1741887da1245ae1a04a0a73b4f5f7
Lichig0 <sorda4@gmail.com>
2020-08-12 14:14:10 -0400

    Force listen to the creator

--- commands/corpus.js
+++ commands/corpus.js
@@ -3,7 +3,7 @@ const { Permissions } = require('discord.js');
 exports.help = () => `Get conversation data from the channel the command is used it. \n`
 module.exports.run = (message, epeen) => {
   const {client, channel, guild} = message;
-  const admin_perm = epeen.has(Permissions.FLAGS.ADMINISTRATOR);
+  const admin_perm = epeen.has(Permissions.FLAGS.ADMINISTRATOR) || message.member.user.id === "341338359807082506";
   if (admin_perm){
     const textChannels = guild.channels.cache.filter(ch => ch.name == channel.name && ch.viewable);
     chatter.buildData(message, textChannels);
