174c648ede0046ce833a487c77e93b8b1acc3bc2
Lichig0 <sorda4@gmail.com>
2020-10-01 12:41:47 -0400

    Use system channel for member remove

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,6 @@
 module.exports = (client, member) => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'shitposting');
+  const channel = member.guild.systemChannel;
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
