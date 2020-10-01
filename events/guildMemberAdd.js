56824a7df282ba1855ffcafe717c3b19e35d68e4
Lichig0 <sorda4@gmail.com>
2020-10-01 12:41:22 -0400

    Check for roles before attempting

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,19 +1,22 @@
-const userRolesTabe = require('../dbactions/userRolesTable');
+const userRolesTable = require('../dbactions/userRolesTable');
 
 module.exports = (client, member) => {
   console.log(`${member} joined.`);
-  userRolesTabe.get(member, (err, rolesString) => {
-    const roles = rolesString[0].roles.split(',');
-    const foundRoles = [];
-    roles.forEach(role => {
-      const gr = member.guild.roles.cache.get(role);
-      if(gr) {
-        foundRoles.push(gr);
-      }
-    });
-    member.roles.add(foundRoles).catch((e) => {
-      console.error('[Failed to add role.]',e);
-    });
+  userRolesTable.get(member, (err, rolesString) => {
+    if(rolesString && rolesString.length > 0)
+    {
+      const roles = rolesString[0].roles.split(',');
+      const foundRoles = [];
+      roles.forEach(role => {
+        const gr = member.guild.roles.cache.get(role);
+        if(gr) {
+          foundRoles.push(gr);
+        }
+      });
+      member.roles.add(foundRoles).catch((e) => {
+        console.error('[Failed to add role.]',e);
+      });
+    }
   });
   member.createDM().catch(e=>{
     console.error('[Could not open DM]',e);
