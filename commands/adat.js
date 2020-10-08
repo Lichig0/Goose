e705b4d95489513519098fad6377faca633fe8c6
Lichig0 <sorda4@gmail.com>
2020-10-08 09:32:49 -0400

    Change order for adat

--- commands/adat.js
+++ commands/adat.js
@@ -4,15 +4,18 @@ module.exports.run = (message) => {
     if (m.kickable === false) {
       return message.reply('ABAT!');
     }
-    m.kick().then(() => {
-      console.log(message, author);
-      channel.createInvite({ maxUses: 1 }).then(invite => {
-        author.createDM().then(dm => {
-          dm.send(`When you're not ADAT: ${invite.url}`);
-        }).catch(error => console.error(error));
-      }).catch(error => console.error(error));
-      channel.send(`ADAT! -${m.tag.tag}`);
-    }).catch(() => message.reply('Error.'));
+    channel.createInvite({ maxUses: 1 }).then(invite => {
+      author.createDM().then(dm => {
+        dm.send(`When you're not ADAT: ${invite.url}`).then(()=> {
+          m.kick().then(() => {
+            console.log(message, author);
+          }).catch(console.error);
+        });
+      }).catch(console.error);
+    }).catch(console.error);
+
+    channel.send(`ADAT! -${m.tag.tag}`);
+
   });
 };
 
