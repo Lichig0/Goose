9ead0371fb0ce087213fc670dda7a7c641708088
Mark <sords4@gmail.com>
2020-11-21 13:37:44 -0500

    Introduce sharding

---
+++ bot.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const fs = require('fs');
+const initTables = require('./dbactions/initTables');
+const settings = require('./settings');
+const client = new Discord.Client({ retryLimit: 3, presence: { activity:{name:'👀', type:'WATCHING'}}});
+
+settings.loadConfig();
+fs.readdir('./events/', (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`);
+    const eventName = file.split('.')[0];
+    client.on(eventName, (...args) => eventHandler(client, ...args));
+  });
+});
+initTables();
+
+client.login(process.env.BOT_TOKEN);
