06458f94b9766f1c9c01950c9d1b61f18c697679
Mark <sords4@gmail.com>
2020-12-21 19:10:49 -0500

    Removed voting from corpus

--- commands/corpus.js
+++ commands/corpus.js
@@ -16,34 +16,8 @@ module.exports.run = (message, epeen) => {
     chatter.buildData(message, textChannels);
   } else {
     const config = settings.settings.chatter;
-    // message.react('🦆');
+    message.react('🦆').catch(console.warn);
     // start polling
-    const filter = (reaction) => (reaction.emoji.name === '👍' || reaction.emoji.name === '👎');
-    const time = (config.voteTime || DEFAULTS.voteTime) * 1000;
-    const requiredVotes = config.requiredVotes || DEFAULTS.requiredVotes;
-
-    message.react('👍').catch(console.error);
-    message.react('👎').catch(console.error);
-
-    const collector = message.createReactionCollector(filter, { time });
-    collector.on('end', collected => {
-      const upVote = collected.get('👍') ? collected.get('👍').count - 1 : 0;
-      const downVote = collected.get('👎') ? collected.get('👎').count - 1 : 0;
-
-      const results = (upVote - downVote) >= requiredVotes || false;
-      message.reactions.removeAll().catch(e => {
-        console.error(e);
-        message.react('👍').catch(console.error);
-        message.react('👎').catch(console.error);
-      });
-      if (results) {
-        message.react('✅');
-        const textChannels = guild.channels.cache.filter(ch => ch.name == channel.name && ch.viewable);
-        chatter.buildData(message, textChannels);
-      } else {
-        message.react('❌');
-      }
-    });
   }
   return;
 };
