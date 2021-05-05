6db2013374d45496f2d60ad6a705194342bdc776
Mark <sords4@gmail.com>
2021-05-05 12:10:02 -0400

    changed indentation in emote

--- commands/emote.js
+++ commands/emote.js
@@ -1,13 +1,13 @@
 const Chance = require('chance');
 const chance = new Chance();
-module.exports.help = () => {'Emote.'};
+module.exports.help = () => { 'Emote.'; };
 
-module.exports.run = (message, epeen) => {
-    const toSay = chance.bool({likelihood: 90});
-    const { client, channel } = message;
-    if(toSay) {
-        channel.send(client.emojis.cache.random().toString()).catch(console.error);
-    } else {
-        message.react(client.emojis.cache.random().id).catch(console.error);
-    }
+module.exports.run = (message) => {
+  const toSay = chance.bool({ likelihood: 90 }); 
+  const { client, channel } = message;
+  if (toSay) {
+    channel.send(client.emojis.cache.random().toString()).catch(console.error);
+  } else {
+    message.react(client.emojis.cache.random().id).catch(console.error);
+  }
 };
