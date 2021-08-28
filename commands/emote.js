0dae52ef5bae64f0b50eb3f8e7f98f2d3e944949
Mark <sords4@gmail.com>
2021-08-28 19:23:35 -0400

    make emote a slash command

--- commands/emote.js
+++ commands/emote.js
@@ -1,13 +1,26 @@
 const Chance = require('chance');
 const chance = new Chance();
+const path = require('path');
+const COMMAND_NAME = path.basename(__filename, '.js');
 module.exports.help = () => { 'Emote.'; };
 
 module.exports.run = (message) => {
-  const toSay = chance.bool({ likelihood: 90 }); 
+  const toSay = chance.bool({ likelihood: 90 });
   const { client, channel } = message;
   if (toSay) {
     channel.send(client.emojis.cache.random().toString()).catch(console.error);
   } else {
     message.react(client.emojis.cache.random().id).catch(console.error);
   }
+};
+
+module.exports.getCommandData = () => {
+  return {
+    name: COMMAND_NAME,
+    description: 'Emote.',
+  };
+};
+
+module.exports.execute = async (client, interaction) => {
+  return interaction.reply(client.emojis.cache.random().toString()).catch(console.error);
 };
