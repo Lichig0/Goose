6311b4db4da308092c6d0d9674049801015d0fc5
Mark <sords4@gmail.com>
2021-09-02 13:33:21 -0400

    turn roles into slash command

--- commands/roles.js
+++ commands/roles.js
@@ -1,5 +1,7 @@
 const { Permissions } = require('discord.js');
 const userRolesTable = require('../dbactions/userRolesTable');
+const path = require('path');
+const COMMAND_NAME = path.basename(__filename, '.js');
 exports.help = () => '(Admin) memorize user roles. \n';
 module.exports.run = (message, epeen) => {
   const admin_perm = epeen.has(Permissions.FLAGS.ADMINISTRATOR);
@@ -13,9 +15,31 @@ module.exports.run = (message, epeen) => {
       });
     } else {
       guild.members.fetch().then(members => {
-        userRolesTable.set(members.array(), guild.id);
+        userRolesTable.set([...members.values()], guild.id);
       });
     }
   }
   return;
 };
+
+module.exports.getCommandData = () => {
+  return {
+    name: COMMAND_NAME,
+    description: 'Admin Util: collect current roles for all users.',
+    default_permission: false,
+    ephemeral: true,
+  };
+};
+
+module.exports.execute = async (client, interaction, epeen) => {
+  const admin_perm = epeen.has(Permissions.FLAGS.ADMINISTRATOR);
+  const {guild} = interaction;
+  await interaction.deferReply();
+  if (admin_perm) {
+    guild.members.fetch().then(members => {
+      userRolesTable.set([...members.values()], guild.id);
+    });
+  }
+  return interaction.editReply('Done.');
+};
+module.exports.dev = false;
