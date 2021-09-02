cf4979373f92ab6306d4c4aa8e838899bfef532f
Mark <sords4@gmail.com>
2021-09-02 13:31:22 -0400

    Turn calc into a slash command

--- commands/calc.js
+++ commands/calc.js
@@ -1,8 +1,36 @@
 const path = require('path');
 const math = require('mathjs');
+const {Constants: {ApplicationCommandOptionTypes}} = require('discord.js');
 
 const COMMAND_NAME = `${path.basename(__filename, '.js')} `;
 const parser = math.parser();
+
+exports.getCommandData = () => {
+  return {
+    name: COMMAND_NAME,
+    description: 'Maths',
+    options: [
+      {
+        name: 'input',
+        description: 'Equation here',
+        type: ApplicationCommandOptionTypes.STRING,
+        required: true
+      }
+    ],
+  };
+};
+
+exports.execute = async (client, interaction) => {
+  const equation = interaction.options.get('input').value;
+  await interaction.deferReply();
+  try {
+    const result = parser.evaluate(equation.trim());
+    interaction.editReply(result.name || result.toString()).catch(console.error);
+  } catch (e){
+    if(e.message) interaction.editReply(e.message).catch(console.error);
+  }
+  return;
+};
 
 exports.help = () => '2 + 2\n';
 
@@ -17,3 +45,5 @@ exports.run = (message) => {
   }
   return;
 };
+
+exports.dev = false;
