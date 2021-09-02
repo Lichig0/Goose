b1b43609cae5a4f8f54911a79901f00c39a36070
Mark <sords4@gmail.com>
2021-09-02 15:01:51 -0400

    make insult a slash command

--- commands/insult.js
+++ commands/insult.js
@@ -2,6 +2,17 @@ const insultTable = require('../dbactions/insultTable');
 const Discord = require('discord.js');
 const settings = require('../settings');
 const Chance = require('chance');
+const path = require('path');
+const COMMAND_NAME = path.basename(__filename, '.js');
+const SUBCOMMANDS = {
+  ADD: 'add',
+  GET: 'get',
+  DELETE: 'delete',
+};
+const PARAMETERS = {
+  INSULT: 'insult',
+  MENTION: 'mention',
+};
 
 const chance = new Chance();
 const insults = [
@@ -9,7 +20,7 @@ const insults = [
   'Swear to God, {member} makes me want to pump nerve gas through the vents.',
   'If I did want a grandchild, I’d just scrape all {member}\'s previous mishaps into a big pile and knit a onesie for it.',
   'So don\'t speak to me. Ever. And while you’re not ever speaking to me; jump up {member}\'s ass and die.',
-  'Do I Get Bonus Points If I Act Like I Care?',
+  'Do I get bonus points if I act like I care?',
   'Don\'t break an arm jerking yourself off.',
   '{member}? It\'s like the n-word and the c-word had a baby and it was raised by all the bad words for Jews.',
   '{member} would suck a dick just to cut in line to suck a bigger dick.',
@@ -18,6 +29,8 @@ const insults = [
   'I can envision millions of Americans rising up as one and demanding legislation that would require {member} legs to be amputated, burned, and buried next to Hitler.',
   '{member} won’t truly appreciate the awkwardness of this moment until they’re fondly reminiscing as a 35-year-old homosexual.',
   'Monetize this corkscrewed cock.',
+  'Please stop talking.',
+  'What is this, amature hour?',
 ];
 const auditHisotry = {};
 let audit = {};
@@ -54,7 +67,7 @@ exports.run = async message => {
   }
 };
 
-const getInsult = function (message, mentioned) {
+const getInsult = function (callback, message, mentioned) {
   const config = settings.settings;
   const mentions = config.mentions || false;
 
@@ -71,13 +84,13 @@ const getInsult = function (message, mentioned) {
     let chat = chance.pickone(fullInsults).replace(/\{member\}/gi, replaceMember);
     // let chat = fullInsults[Math.floor(Math.random() * fullInsults.length)].replace(/\{member\}/gi, replaceMember);
     if (!mentions) chat = Discord.Util.cleanContent(chat, message);
-    return chat;
+    callback(chat);
   });
 };
 
 const getRandomUsers = (message) => {
-  const members = message.channel.members;
-  return [...members.values()][Math.floor(Math.random() * members.length)];
+  const members = [...message.guild.channels.cache.get(message.channelId)?.members?.values()];
+  return members[Math.floor(Math.random() * members.length)];
 };
 
 const sendInsult = (channel, text, options) => {
@@ -89,4 +102,75 @@ const sendInsult = (channel, text, options) => {
   }).catch(console.error);
 };
 
+exports.execute = async (client, interaction) => {
+  await interaction.deferReply();
+
+  const { user, options, guild, } = interaction;
+  if (options.getSubcommand() == SUBCOMMANDS.ADD) {
+    let insult = options.get(PARAMETERS.INSULT);
+    // console.log(insult)
+    insultTable.insert(insult, user.id, guild.id, err => {
+      if(err) {
+        interaction.editReply('Failed to add insult. I didn\t fail, you did.');
+      } else {
+        interaction.editReply('Added. Someone hates you for that.');
+      }
+    });
+  }
+  else {
+    const mentionable = options.get(PARAMETERS.MENTION);
+    const a = audit;
+    const reply = (text) => {
+      interaction.editReply(text, options).then((sentMessage) => {
+        a.timestamp = Date.now();
+        auditHisotry[sentMessage.id] = a;
+      }).catch(console.error);
+    };
+    if (mentionable) {
+      const members = mentionable.role ? [...mentionable.role.members.values()] : [mentionable.member];
+      members.each(member => {
+        getInsult(reply, interaction, member);
+      });
+    } else {
+      getInsult(reply, interaction);
+    }
+  }
+};
+
+exports.getCommandData = () => {
+  return {
+    name: COMMAND_NAME,
+    description: 'Say an insult, maybe even drop a mention?',
+    options: [
+      {
+        name: SUBCOMMANDS.ADD,
+        description: 'Add an insult.',
+        type: Discord.Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+        options: [
+          {
+            name: PARAMETERS.INSULT,
+            description: 'Come up with an insult. Try using `{member}` in place of someone\'s name',
+            type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+            required: true
+          }
+        ]
+      },
+      {
+        name: SUBCOMMANDS.GET,
+        description: 'Say an insult.',
+        type: Discord.Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+        options: [
+          {
+            name: PARAMETERS.MENTION,
+            description: 'Insult who now?',
+            type: Discord.Constants.ApplicationCommandOptionTypes.MENTIONABLE,
+            required: false,
+          }
+        ]
+      }
+    ]
+  };
+};
+
+exports.dev = false;
 exports.getInsult = getInsult;
