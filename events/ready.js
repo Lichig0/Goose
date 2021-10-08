6f680e40cd3f7581535601ab540da41a78319801
Mark <sords4@gmail.com>
2021-10-08 16:08:57 -0400

    Check for optOut for initializing chatter

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,13 @@
 const chatter = require('../chatter/chatter');
+const optOutTable = require('../dbactions/optOutTable');
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
-  chatter.init(client);
+  optOutTable.get((err, ids) => {
+    if(err) {
+      console.warn('[OptOut]', err.message);
+    }
+    client.optedOutUsers = ids;
+    console.log('[OptOut]', client.optedOutUsers);
+    chatter.init(client);
+  });
 };
