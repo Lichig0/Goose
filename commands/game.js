fd4ab02dd079c66ecde5365c551ad8b30fd8d36e
Mark <sords4@gmail.com>
2021-10-20 11:42:58 -0400

    added game id, and game random

--- commands/game.js
+++ commands/game.js
@@ -4,9 +4,111 @@ const GiantBomb = require('giant-bomb');
 const gb = new GiantBomb(process.env.GIANT_BOMB_KEY, 'Goose bot game search for Bounty Board. Retrieve game info for bounties.');
 
 const COMMAND_NAME = path.basename(__filename, '.js');
+const SUB_COMMAND = {
+  SEARCH: 'search',
+  GET: 'get',
+  RANDOM: 'random'
+};
+const OPTIONS = {
+  NAME: 'name',
+  ID: 'id',
+};
 
-module.exports.getGame = (callback, id) => {
-  const guid = id || Math.floor(Math.random() * 90000);
+const searchGame = (searchString, callback) => {
+  const options = {
+    query: searchString,
+    fields: ['name','guid', 'image', 'aliases', 'deck',
+      'description','original_release_date', 'site_detail_url',
+      'platforms', 'expected_release_day', 'expected_release_month',
+      'expected_release_year', 'expected_release_quarter'],
+    resources: ['game']
+  };
+
+  gb.search(options).then(response => {
+    const j = JSON.parse(response);
+    const games = j.results;
+    const game = games[0];
+    const embed = new MessageEmbed();
+    if(game === undefined) return callback(embed.setTitle('No Results.'));
+    const { name, guid, image, aliases, deck,
+      original_release_date, site_detail_url,
+      platforms, expected_release_day, expected_release_month,
+      expected_release_year, expected_release_quarter } = game;
+
+    let plats = [];
+    platforms.forEach(platform => plats.push(platform.name));
+
+    let otherGames = [];
+    games.forEach( (g, i) => {
+      if(i > 0 && i <= 10) {
+        otherGames.push(g.name);
+      }
+    });
+
+    embed.setTitle(name).setDescription(deck).setURL(site_detail_url);
+    embed.setFooter(guid);
+    embed.setThumbnail(image.original_url);
+    embed.addField('Platforms', `${plats}`, true);
+    if(aliases) embed.addField('Alias(es)', aliases);
+    if(original_release_date) embed.addField('Released', original_release_date, true);
+    if (!original_release_date) embed.addField('Expected release', `${expected_release_year || '????'} - ${expected_release_month || '??' } - ${expected_release_day || '??'} Q${expected_release_quarter || '?'}`);
+    if (otherGames.toString() !== '')embed.addField('More results', otherGames.toString(), true);
+    // interaction.editReply({embeds: [embed]}).catch(console.warn);
+    return callback(embed);
+  }).catch(console.error);
+};
+
+const getGame = (id, callback) => {
+  const options = {
+    id: id ?? Math.floor(Math.random() * 90000),
+    format: 'json',
+    fields: [
+      'name', 'guid', 'image', 'aliases', 'deck', 'platforms', 'similar_games', 'themes', 'site_detail_url',
+      'genres', 'franchises', 'characters', 'concepts', 'developers', 'original_release_date',
+      'expected_release_year', 'expected_release_month', 'expected_release_day', 'expected_release_quarter'
+    ]
+  };
+  gb.getGame(options).then( response => {
+    const embed = new MessageEmbed();
+    if(response === null) { return callback(embed.setTitle('Not found.')); }
+    const json = JSON.parse(response);
+    if (json.status_code !== 1) { return callback(embed.setTitle('Error').setDescription(json.error)); }
+    const {name, guid, image, aliases, deck, platforms, similar_games, site_detail_url,
+      genres, franchises, developers, original_release_date,
+      expected_release_year, expected_release_month, expected_release_day, expected_release_quarter
+    } = json.results;
+
+    let plats = [];
+    platforms ? platforms.forEach(platform => plats.push(platform.name)) : plats.push('No data.');
+    let similarGames = [];
+    similar_games ? similar_games.forEach(similar => similarGames.push(similar.name)) : similarGames.push('No data.');
+    let devs = [];
+    developers ? developers.forEach(dev => devs.push(dev.name)) : devs.push('No data.') ;
+    let frans = [];
+    franchises ? franchises.forEach(franchise => frans.push(franchise.name)) : frans.push('No data.');
+    let genre = [];
+    genres ? genres.forEach(g => genre.push(g.name)) : genre.push('No data.');
+
+    embed.setTitle(name).setDescription(deck).setURL(site_detail_url);
+    embed.setFooter(guid);
+    embed.setThumbnail(image.original_url);
+    if (original_release_date) embed.addField('Released', original_release_date, true);
+    if (!original_release_date) embed.addField('Expected release', `${expected_release_year || '????'} - ${expected_release_month || '??'} - ${expected_release_day || '??'} Q${expected_release_quarter || '?'}`);
+    if (aliases) embed.addField('Alias(es)', aliases);
+    embed.addFields([
+      { name: 'Similar Games', value: `${similarGames}`, inline: true },
+      { name: 'Platforms', value: `${plats}`, inline: true},
+      { name: 'Developers', value: `${devs}`, inline: true },
+      { name: 'Franchise', value: `${frans}`, inline: true },
+      { name: 'Genres', value: `${genre}`, inline: true },
+    ]);
+    callback(embed);
+  }).catch(console.error);
+
+};
+
+module.exports.getGame = (id, callback) => {
+  const guid = id ?? Math.floor(Math.random() * 90000);
   const options = {
     id: guid,
     format: 'json',
@@ -24,6 +126,7 @@ module.exports.getGame = (callback, id) => {
     callback(json.results);
   });
 };
+
 module.exports.getCommandData = () => {
   return {
     type: ApplicationCommandTypes.CHAT_INPUT,
@@ -32,57 +135,60 @@ module.exports.getCommandData = () => {
     default_permission: true,
     options: [
       {
-        name: 'game',
-        type: ApplicationCommandOptionTypes.STRING,
-        description: 'Game name to search for',
-        required: true
+        name: SUB_COMMAND.SEARCH,
+        type: ApplicationCommandOptionTypes.SUB_COMMAND,
+        description: 'Search for a game',
+        options: [
+          {
+            name: OPTIONS.NAME,
+            type: ApplicationCommandOptionTypes.STRING,
+            description: 'Game name to search for',
+            required: true
+          },
+        ]
+      },
+      {
+        name: SUB_COMMAND.GET,
+        type: ApplicationCommandOptionTypes.SUB_COMMAND,
+        description: 'Get a specific game',
+        options: [
+          {
+            name: OPTIONS.ID,
+            type: ApplicationCommandOptionTypes.STRING,
+            description: 'GiantBomb Game ID to get. (Default: Random)',
+            required: false,
+          }
+        ]
+      },
+      {
+        name: SUB_COMMAND.RANDOM,
+        type: ApplicationCommandOptionTypes.SUB_COMMAND,
+        description: 'Get a random Game'
       }
     ]
   };
 };
+
 module.exports.execute = async (client, interaction) => {
-  await interaction.deferReply();
+  await interaction.deferReply().catch(console.warn);
+  // const randGid = Math.floor(Math.random() * 90000);
 
-  const gameName = interaction.options.get('game').value;
-  const options = {
-    query: gameName,
-    fields: ['name','guid', 'image', 'aliases', 'deck',
-      'description','original_release_date', 'site_detail_url',
-      'platforms', 'expected_release_day', 'expected_release_month',
-      'expected_release_year', 'expected_release_quarter'],
-    resources: ['game']
-  };
-
-  gb.search(options).then(response => {
-    const j = JSON.parse(response);
-    const games = j.results;
-    const game = games[0];
-    if(game === undefined) return interaction.editReply('No result.').catch(console.warn);
-    const { name, guid, image, aliases, deck,
-      original_release_date, site_detail_url,
-      platforms, expected_release_day, expected_release_month,
-      expected_release_year, expected_release_quarter } = game;
-
-    let plats = [];
-    platforms.forEach(platform => plats.push(platform.name));
-
-    let otherGames = [];
-    games.forEach( (g, i) => {
-      if(i > 0 && i <= 10) {
-        otherGames.push(g.name);
-      }
+  const subCommand = interaction.options.getSubcommand();
+  console.log(subCommand, interaction.options);
+  switch (subCommand) {
+  case SUB_COMMAND.SEARCH:
+    searchGame(interaction.options.get('name')?.value, (embed) => {
+      interaction.editReply({embeds: [embed]}).catch(console.warn);
     });
-
-    const embed = new MessageEmbed();
-    embed.setTitle(name).setDescription(deck).setURL(site_detail_url);
-    embed.setFooter(guid);
-    embed.setThumbnail(image.original_url);
-    embed.addField('Platforms', `${plats}`, true);
-    if(aliases) embed.addField('Alias(es)', aliases);
-    if(original_release_date) embed.addField('Released', original_release_date, true);
-    if (!original_release_date) embed.addField('Expected release', `${expected_release_year || '????'} - ${expected_release_month || '??' } - ${expected_release_day || '??'} Q${expected_release_quarter || '?'}`);
-    if (otherGames.toString() !== '')embed.addField('More results', otherGames.toString(), true);
-    interaction.editReply({embeds: [embed]}).catch(console.warn);
-  }).catch(console.error);
+    break;
+  case SUB_COMMAND.GET:
+  case SUB_COMMAND.RANDOM:
+  default:
+    getGame(interaction.options.get(OPTIONS.ID)?.value, (embed) => {
+      interaction.editReply({embeds: [embed]}).catch(console.warn);
+    });
+    break;
+  }
 };
+
 module.exports.dev = false;
