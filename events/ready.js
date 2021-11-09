3c83e42b23cb4c934497f81f03da2746500e0961
Mark <sords4@gmail.com>
2021-11-09 12:32:16 -0500

    Send DM when bot starts.

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,13 @@ const chatter = require('../chatter/chatter');
 const optOutTable = require('../dbactions/optOutTable');
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
+  process.title = client.user.tag;
+  client.owner = process.env.OWNER;
+  client.users.fetch(`${client.owner}`).then(user => {
+    user.createDM().then(dmChannel => {
+      dmChannel.send(`${client.user.tag} has started.`);
+    }).catch(console.error);
+  }).catch(console.error);
   optOutTable.get((err, ids) => {
     if(err) {
       console.warn('[OptOut]', err.message);
