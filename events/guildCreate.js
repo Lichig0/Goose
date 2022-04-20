8aa218091ea6411429d447e93ad4c701896213ab
Mark <sords4@gmail.com>
2022-04-20 11:28:50 -0400

    Notify user of Guild Joins

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,6 +2,12 @@ const chatter = require('../chatter/chatter');
 
 module.exports = (client, guild) => {
   console.log(`[Joined guild] ${guild.name}`);
+  client.owner = process.env.OWNER;
+  client.users.fetch(`${client.owner}`).then(user => {
+    user.createDM().then(dmChannel => {
+      dmChannel.send(`${client.user.tag} has joined ${guild.name}.`);
+    }).catch(console.error);
+  }).catch(console.error);
   chatter.addGuildBrain(guild).then((guildData) => {
     console.log('[Finished scraping.]', guildData.length);
   }).catch(console.error);
