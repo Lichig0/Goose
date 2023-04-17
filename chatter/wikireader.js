e07fed5e332f7ef78c64733fd64e80ad515f44b0
Mark <sords4@gmail.com>
2023-04-17 09:27:53 -0400

    ability to ingest multiple wiki pages

--- chatter/wikireader.js
+++ chatter/wikireader.js
@@ -1,42 +1,49 @@
 const Chance = require('chance');
-const Markov = require('markov-strings').default;
+// const Markov = require('markov-strings').default;
+const Markov = require('markov-flexible');
 const wiki =  require('wikipedia');
 const chatterUtil = require('./util');
 
+// eslint-disable-next-line no-control-regex
 const TRIMMER = new RegExp('==.*==|\n|^$', 'g');
 const PUNCTUATION = new RegExp('[^.!?]*[.!?]', 'g');
 
 const chance = new Chance();
-const markov = new Markov({ stateSize: 2 });
 const minimumScore = 2; // TODO: grab from config
+const markov = new Markov.MarkovChain(2);
 const wikiGenOptions = {
-  maxTries: 10,
+  retries: 750,
   filter: (r) => {
-    const multiRef = r.refs.length;
     const goodLength = chatterUtil.wordScore(r.string);
-    return (multiRef + goodLength) >= minimumScore && !r.refs.includes(r.string);
+    return goodLength >= minimumScore;
   }
 };
 
-const getWikiDaily = async() => {
-  try {
-    const events = Object.values(await wiki.onThisDay({type: 'events'}))[0];
-    const pageSummary = chance.pickone(Object.values(events)[0].pages);
-    const page = await wiki.page(pageSummary.title);
-    const content = await page.content();
-    return(content);
-  } catch (error) {
-    console.log(error);
+const getWikiDaily = async (times = 1) => {
+  for(let i = 0; i <= times; i++) {
+    try {
+      const events = Object.values(await wiki.onThisDay({type: 'events'}))[0];
+      const pageSummary = chance.pickone(Object.values(events)[0].pages);
+      const page = await wiki.page(pageSummary.title);
+      const content = await page.content();
+      return(content);
+    } catch (error) {
+      console.log(error);
+      break;
+    }
   }
 };
 
-const getRandomWiki = async () => {
-  try {
-    const pageSummary = await wiki.random();
-    const page = await wiki.page(pageSummary.title);
-    return page.content();
-  } catch (error) {
-    console.error(error);
+const getRandomWiki = async (times = 1) => {
+  for(let i = 0; i <= times; i++) {
+    try {
+      const pageSummary = await wiki.random();
+      const page = await wiki.page(pageSummary.title);
+      return page.content();
+    } catch (error) {
+      console.error(error);
+      break;
+    }
   }
 };
 
@@ -49,20 +56,22 @@ const trimContentToStringArray = function (wikiContent) {
 module.exports.addDailyWiki = async () => {
   const wikiContent = await getWikiDaily();
   const sentences = trimContentToStringArray(wikiContent);
-  markov.addData(sentences);
+  markov.addString(sentences);
 };
 
 module.exports.addRandomWiki = async () => {
   const wikiContent = await getRandomWiki();
   const sentences = trimContentToStringArray(wikiContent);
-  markov.addData(sentences);
+  markov.addString(sentences);
 };
 
-module.exports.generateWikiSentence = (options = wikiGenOptions) => {
+module.exports.generateWikiSentence = async (options = wikiGenOptions) => {
+  const result = await markov.generateSentence(options);
   return new Promise((resolve, reject) => {
     try {
-      const sentence = markov.generate(options);
-      resolve(sentence);
+      result.string = result.text;
+      result.refs = Object.values(result.refs);
+      resolve(result);
     } catch (e) {
       reject(`Could not generate sentence ${e}`);
     }
