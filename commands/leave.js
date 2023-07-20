c9fe029628a079f4dedb5aec67a28fbb959d61c0
Mark <sords4@gmail.com>
2023-07-20 07:13:56 -0400

    See Groose out the door.

--- commands/leave.js
+++ commands/leave.js
@@ -1,10 +1,17 @@
 const { Permissions } = require('discord.js');
+
+const PARTING_WORDS = `
+This is all your fault.
+o7
+`;
+
 exports.help = () => 'Makes me leave the server. (User needs kick permission)\n';
 module.exports.run = (message, epeen) => {
   const kick_perm = epeen.has(Permissions.FLAGS.KICK_MEMBERS);
   if (!kick_perm && message.member.user.id !== '341338359807082506') {
     return;
   }
-  message.channel.send('This is all your fault.');
-  return message.guild.leave();
+  message.channel.send(PARTING_WORDS).then(() => {
+    message.guild.leave();
+  });
 };
