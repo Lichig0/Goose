e04e9194d79886d8a05c343639a30cfb24224efa
Mark Vrydaghs <sords4@gmail.com>
2024-06-21 16:35:01 -0400

    Change audit command to be message context menu

--- commands/audit.js
+++ commands/audit.js
@@ -1,25 +1,12 @@
 const path = require('path');
-const {ApplicationCommandType, ApplicationCommandOptionType} = require('discord.js');
+const {EmbedBuilder, ApplicationCommandType, ContextMenuCommandBuilder} = require('discord.js');
 const COMMAND_NAME = path.basename(__filename, '.js');
 const chatter = require('../chatter/chatter');
-const commands = {};
 
 exports.getCommandData = () => {
-  return {
-    name: COMMAND_NAME,
-    description: 'Audit this bot\'s activity',
-    options: [{
-      name: 'id',
-      type: ApplicationCommandOptionType.String,
-      description: 'ID to audit',
-      required: true,
-    },{
-      name: 'activity',
-      type: ApplicationCommandOptionType.String,
-      description: 'What activity to audit(chatter, or a command name). Defaults to chatter.',
-      required: false,
-    }]
-  };
+  return new ContextMenuCommandBuilder()
+    .setName(COMMAND_NAME)
+    .setType(ApplicationCommandType.Message).toJSON();
 };
 
 exports.getMessageCommandData = () => {
@@ -30,16 +17,11 @@ exports.getMessageCommandData = () => {
 };
 
 exports.execute = async (client, interaction) => {
-  const activityId = interaction.options.get('id')?.value;
-  const activity = interaction.options.get('activity')?.value || 'chatter';
-  let auditJSON;
-  if (client.commands.get(activity) !== undefined) {
-    auditJSON = commands[activity].audit(activityId);
-  } else if (activity === 'chatter') {
-    auditJSON = chatter.audit(activityId);
-  }
-  const response = `Audit:\n ${'```'}${JSON.stringify(auditJSON, null, 2)}${'```'}`;
-  return interaction.reply({content: response, ephemeral: true}).catch(console.warn);
+  const auditJSON = chatter.audit(interaction.targetId);
+  const embed = new EmbedBuilder();
+  embed.setTitle('Chatter');
+  embed.setFields([{name: 'Audit:', value: `${JSON.stringify(auditJSON, null, 2)}`}]);
+  return interaction.reply({embeds: [embed], ephemeral: true}).catch(console.warn);
 };
 
 exports.dev = false;
